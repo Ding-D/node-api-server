00e807ea3ac50813cd4df870296d4d1664b66606
Ding-D <m18220798994@163.com>
2021-01-27 13:41:04 +0800

    over

---
+++ db/mydb.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : MySQLDD
+Source Server Version : 80019
+Source Host           : localhost:3306
+Source Database       : vue_shop
+
+Target Server Type    : MYSQL
+Target Server Version : 80019
+File Encoding         : 65001
+
+Date: 2020-08-25 13:16:01
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
